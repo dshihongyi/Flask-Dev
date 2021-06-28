eec4182c57370f266b782b500babaf4785c04eff
dshihongyi <shihongyivc@hotmail.com>
2021-06-28 14:41:12 +1200

    no Log

--- mysql.sh
+++ mysql.sh
@@ -7,3 +7,5 @@ sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON myflaskapp.* TO 'daniel'@'localhos
 sudo mysql -uroot -e "USE myflaskapp; CREATE TABLE users(id INT(11) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), email VARCHAR (100), username VARCHAR(30), password VARCHAR(100), register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
 sudo mysql -uroot -e "USE myflaskapp; CREATE TABLE isp_templates (id INT(11) AUTO_INCREMENT PRIMARY KEY, isp VARCHAR(50), type VARCHAR(50), model VARCHAR(50), site VARCHAR(15), ci_name VARCHAR(50), config longtext, last_editor VARCHAR(100), create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
 sudo python3 ~/Flask-Dev/app.py
+exit 0
+
